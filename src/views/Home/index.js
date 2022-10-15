49e013e440850be06014972c7b152dd1d2207af3
Dani-linus <dns40625@gmail.com>
2022-10-15 11:43:45 -0300

    commit

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -13,7 +13,7 @@ export default function HomeView({ navigation }) {
 
     const { initSound  } = useContext(SoundContext);
 
-    initSound();
+        initSound();
 
     return (
         <View style={styles.container}>
